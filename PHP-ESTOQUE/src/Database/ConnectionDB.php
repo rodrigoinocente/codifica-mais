ec52ee412c50431fffae28a9365c55bf990d259a
Rodrigo Inocente <ridrigocorreia@gmail.com>
2026-02-15 12:02:52 -0300

    Ajustar ConnectionDB

--- PHP-ESTOQUE/src/Database/ConnectionDB.php
+++ PHP-ESTOQUE/src/Database/ConnectionDB.php
@@ -2,34 +2,29 @@
 
 namespace App\Database;
 
-use Exception;
 use PDO;
-use PDOException;
 
 class ConnectionDB
 {
-    private static $instance = null;
+    private static PDO|null $instance;
 
-    public static function conectar()
+    public static function conectar(): PDO
     {
-        try {
-            $host = "localhost";
-            $db   = "sistema_login";
-            $usuario = "codifica-mais";
-            $senha = "123456";
+      $host = "localhost";
+      $db   = "sistema_login";
+      $usuario = "codifica-mais";
+      $senha = "123456";
 
-            self::$instance = new PDO(
-                "mysql:host=$host;dbname=$db;charset=utf8mb4",
-                $usuario,
-                $senha,
-                [
-                    PDO::ATTR_ERRMODE,
-                    PDO::ERRMODE_EXCEPTION,
-                ]
-            );
-        } catch (PDOException $e) {
-            throw new Exception("Estamos com problemas. Tente mais tarde.");
-        }
+      self::$instance = new PDO(
+        "mysql:host=$host;dbname=$db;charset=utf8mb4",
+          $usuario,
+          $senha,
+          [
+              PDO::ATTR_ERRMODE,
+              PDO::ERRMODE_EXCEPTION,
+          ]
+        );
+
         return self::$instance;
-    }
+      }
 }
